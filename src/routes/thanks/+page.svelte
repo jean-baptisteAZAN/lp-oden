60b1210c21539191842205d4fc8eac1d783f3885
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-08-17 16:08:21 +0200

    feat(navbar): calendly

---
+++ src/routes/thanks/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-full py-40 flex flex-col items-center justify-center">
+	<h1 class="h1 text-center">
+		Merci d'avoire pris le temps de nous contacter.
+		<br/>Nous avons hâte de vous parler de notre solution.
+	</h1>
+	<img src="/Students-bro.svg" alt="Merci" class="w-1/2"/>
+</div>
